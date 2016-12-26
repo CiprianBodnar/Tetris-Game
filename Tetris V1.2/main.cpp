722d4ce519968c17120349321c7a54b4cf4dc350
CiprianBodnar <cyprian.bodnar@gail.com>
2016-12-26 02:24:37 +0200

    Delay and mouse clik

--- Tetris V1.2/main.cpp
+++ Tetris V1.2/main.cpp
@@ -1,6 +1,7 @@
 #include <SFML/Graphics.hpp>
 #include<iostream>
 #include "movent.h"
+
 using namespace std;
 using namespace sf;
 
@@ -9,26 +10,55 @@ using namespace sf;
 int main()
 {
 	RenderWindow window(VideoMode(1024, 720), "Tetris V1.2");
+
+	RectangleShape jucator;
+	jucator.setPosition(500, 500);
+	jucator.setFillColor(Color::Black);
+	jucator.setSize(Vector2f(20,30));
+
+	
 	Texture tetrisPices;
-	tetrisPices.loadFromFile("IMG/miniSquer.png");
+	tetrisPices.loadFromFile("IMG/tiles.png");
 	Sprite sprite(tetrisPices);
-	
 	sprite.setPosition(24, 24);
-	Vector2f origineCurenta;
-	
+	sprite.setTextureRect(IntRect(0, 0, 18, 18));
+	sprite.setOrigin(9, 9);
+	Vector2f sizeOfTetrisPices;
+
+	/*sizeOfTetrisPices = 
+	cout << sizeOfTetrisPices.x << " " << sizeOfTetrisPices.y << endl;*/
+
+
+
+	Vector2u windowActualSize;
+	windowActualSize = window.getSize();
+	bool ok = 1;
 	while (window.isOpen())
 	{
+		
+
 		Event event;
 		while (window.pollEvent(event))
 		{
 			if (event.type == Event::Closed)
 				window.close();
+			
 		}
 		
-		move(sprite);
+		if(ok==1)
+		moveDown(sprite);
+		if (sprite.getOrigin().y + sprite.getPosition().y >500.f)
+			ok = 0;
+		else ok = 1;
+		if (Mouse::getPosition(window).x > 500 && Mouse::getPosition(window).y > 500 && Mouse::getPosition(window).x < 520 && Mouse::getPosition(window).y < 530)
+			if (Mouse::isButtonPressed(Mouse::Left))
+				cout << "buton patratel apasat";
 			
+		
+		move(sprite,windowActualSize.x,windowActualSize.y);
 		window.clear(Color::White);
 		window.draw(sprite);
+		window.draw(jucator);
 		window.display();
 	}
 
--- Tetris V1.2/movent.h
+++ Tetris V1.2/movent.h
@@ -1,18 +1,29 @@
 #include<SFML\Graphics.hpp>
-using namespace sf;
 
+using namespace sf;
+using namespace std;
 float normalSpeed = 0.25f;
 
-void move(Sprite &sprite)
+Vector2f elementPosition, elementOrigin;
+void move(Sprite &sprite, unsigned windowXsize,unsigned windowYsize)
 {
-
-	if (Keyboard::isKeyPressed(Keyboard::A))
-		sprite.move(Vector2f(-normalSpeed, 0.0f));
-	if (Keyboard::isKeyPressed(Keyboard::D))
+	
+	elementPosition = sprite.getPosition();
+	elementOrigin = sprite.getOrigin();
+	if (Keyboard::isKeyPressed(Keyboard::D) )
+		if(elementPosition.x+elementOrigin.x<windowXsize)
 		sprite.move(Vector2f(normalSpeed, 0.0f));
-	if (Keyboard::isKeyPressed(Keyboard::S))
+	if (Keyboard::isKeyPressed(Keyboard::S) )
+		if(elementPosition.y+elementOrigin.y<windowYsize)
 		sprite.move(Vector2f(0.0f, normalSpeed));
 	if (Keyboard::isKeyPressed(Keyboard::W))
 		sprite.move(Vector2f(0.0f, -normalSpeed));
-
+	if (Keyboard::isKeyPressed(Keyboard::A))
+		if(elementPosition.x+elementOrigin.x>0)
+		sprite.move(Vector2f(-normalSpeed,0.0f ));
+}
+float normalSpeedOfPiece = 0.05f;
+void moveDown(Sprite &sprite)
+{
+	sprite.move(Vector2f(0.0f, normalSpeedOfPiece));
 }
